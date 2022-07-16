ab5808a41eb011515abbb6f9d40476a1462247fe
Santiago Podesta <santi.podesta.c@gmail.com>
2022-07-16 18:03:18 -0300

    algunas cosas simples de las clases

--- clases/cliente_black.py
+++ clases/cliente_black.py
@@ -17,8 +17,11 @@ class Black(Cliente):
         else:
             return True
 
-    def puede_crear_tarjeta_de_credito(self) -> bool:
-        return True
+    def puede_crear_tarjeta_de_credito(self, cant_tarjetas) -> bool:
+        if cant_tarjetas >= 5:
+            return False
+        else:
+            return True
 
     def puede_comprar_dolar(self) -> bool:
         return True
--- clases/cliente_gold.py
+++ clases/cliente_gold.py
@@ -1,8 +1,9 @@
 import json
 from cliente import Cliente
 
+
 class Gold(Cliente):
-    
+
     def __init__(self) -> None:
         with open("eventos_gold.json") as jsonFile:
             archivo = json.load(jsonFile)
@@ -10,11 +11,17 @@ class Gold(Cliente):
             super().__init__(archivo, {'limite_extraccion_diario': 20000, 'limite_transferencia_recibida': 500000,
                                        'costo_transferencia': 0.5})
 
-    def puede_crear_chequera(self) -> bool:
-        return True
+    def puede_crear_chequera(self, cant_chequeras) -> bool:
+        if cant_chequeras >= 1:
+            return False
+        else:
+            return True
 
-    def puede_crear_tarjeta_de_credito(self) -> bool:
-        return True
+    def puede_crear_tarjeta_de_credito(self, cant_tarjetas) -> bool:
+        if cant_tarjetas >= 1:
+            return False
+        else:
+            return True
 
     def puede_comprar_dolar(self) -> bool:
         return True
